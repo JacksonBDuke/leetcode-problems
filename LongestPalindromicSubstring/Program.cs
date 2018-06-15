b6785ec8520f037835b04e7d332670836870382f
Jackson Duke <JacksonBDuke@gmail.com>
2018-06-15 17:27:57 -0500

    Added LongestPalindromicSubstring and ZigZagConversion from other directory.

---
+++ LongestPalindromicSubstring/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace LongestPalindromicSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testCase = "bananas";
+            string testCase2 = "aaaaasaaaaaa";
+            Console.WriteLine(LongestPalindrome(testCase));
+            Console.WriteLine(LongestPalindrome(testCase2));
+            Console.WriteLine(GivenSolution(testCase));
+            Console.WriteLine(GivenSolution(testCase2));
+        }
+
+        public static string LongestPalindrome(string s){
+
+            string longestPalindrome = "";
+            string temporaryPalindrome = "";
+            int leftIndex, rightIndex, tempRightIndex;
+            leftIndex = rightIndex = tempRightIndex = 0;
+
+            while(rightIndex < s.Length){
+
+                while(rightIndex < s.Length && s[leftIndex] == s[rightIndex]){
+                    temporaryPalindrome = s.Substring(leftIndex, (1 + rightIndex - leftIndex));
+                    ++rightIndex;
+                }
+
+                if(leftIndex > 0) --leftIndex;
+                tempRightIndex = rightIndex;
+                
+                while((leftIndex >= 0 && tempRightIndex < s.Length) && s[leftIndex] == s[tempRightIndex]){
+                    temporaryPalindrome = s.Substring(leftIndex, (1 + tempRightIndex - leftIndex));
+                    ++tempRightIndex;
+                    --leftIndex;
+                }
+
+                leftIndex = rightIndex;
+
+                if(temporaryPalindrome.Length > longestPalindrome.Length) longestPalindrome = temporaryPalindrome;
+            }
+
+            return longestPalindrome;
+        }
+        public static string GivenSolution(string s){
+            int start = 0, end = 0;
+            for(int i = 0; i < s.Length; i++){
+                int len1 = expandAroundCenter(s, i, i);
+                int len2 = expandAroundCenter(s, i, i + 1);
+                int len = Math.Max(len1, len2);
+                if(len > end - start){
+                    start = i - (len - 1)/2;
+                    end = i + len/2;
+                }
+            }
+            return s.Substring(start, end + 1);
+        }
+
+        private static int expandAroundCenter(string s, int left, int right){
+            int L = left, R = right;
+            while(L >= 0 && R < s.Length && s[L] == s[R]){
+                L--;
+                R++;
+            }
+            return R - L - 1;
+        }
+    }
+}
---
+++ ZigZagConversion/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ZigZagConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString1 = "PAYPALISHIRING";
+            string solutionCase1 = "PINALSIGYAHRPI";
+
+            ZigZagString testCase1 = new ZigZagString(testString1, 4);
+            testCase1.Print();
+        }
+        
+        public class ZigZagString{
+
+            private string convertedString;
+            public string ConvertedString{
+                get{return convertedString;}
+            }
+
+            private int numRows;
+            public int NumRows{
+                get{return numRows;}
+            }
+
+            public ZigZagString(string s, int n){
+                convertedString = Convert(s, n);
+                numRows = n;
+            }
+            
+            private string Convert(string s, int numRows){
+                return "test";
+            }
+
+            public void Print(){
+                int spacer = this.NumRows - 2 >= 0 ? this.NumRows - 2: 0;
+                int numCharsToPrint = 1;
+                int numSpacesToPrint = spacer;
+                int columns = calculateColumns(spacer);
+                
+                string [,,] zigZagArray = new string[this.NumRows, columns];
+
+                
+
+                int calculateColumns(int maxInSpaces){
+                    int rowsAndSpaces = this.numRows + maxInSpaces;
+                    int numFullyPopulatedColumsAndSpacers = this.ConvertedString.Length / rowsAndSpaces;
+                    int numCharactersLeft = this.ConvertedString.Length % rowsAndSpaces;
+
+                    return numCharactersLeft > 0 ? numFullyPopulatedColumsAndSpacers + 1 : numFullyPopulatedColumsAndSpacers;
+                }
+            }
+        }
+    }
+}
